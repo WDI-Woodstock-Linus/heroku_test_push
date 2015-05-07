7502cc60a27fca6094a4954fcd028206418f548c
Tom Taylor <tom.taylor@outlook.com>
2015-05-07 15:10:01 -0500

    added show route PillsController

--- suppository/app/controllers/pills_controller.rb
+++ suppository/app/controllers/pills_controller.rb
@@ -1,17 +1,21 @@
 class PillsController < ApplicationController
+
   def index
     #Anna do
   end
 
   def show
     #Tom do
+    @pill = Pill.find(params[:id])
   end
 
   def new
-    #Tom do
+    #Tom
+    @pill = Pill.new
   end
 
   def edit
     #Matt do
   end
+
 end

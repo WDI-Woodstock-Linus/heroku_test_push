e55c7fcf7481876b5b516a8eefc97e97d93e1ed5
Tom Taylor <tom.taylor@outlook.com>
2015-05-07 16:09:46 -0500

    completed pill controller

--- suppository/app/controllers/pills_controller.rb
+++ suppository/app/controllers/pills_controller.rb
@@ -6,11 +6,16 @@ class PillsController < ApplicationController
   end
 
   def show
-    #Tom do
+    @pill = Pill.find(params[:id])
   end
 
   def new
     @pill = Pill.new
+  end
+
+  def create
+    pill = Pill.create( pill_params)
+    redirect_to "pills/#{ user.id }"
   end
 
   def edit
@@ -18,9 +23,20 @@ class PillsController < ApplicationController
     #Matt do
   end
 
+  def update
+  pill = Pill.find(params[:id])
+  pill.update!( pill_params )
+  redirect_to "/pills/#{ pill.id }"
+end
+
+def destroy
+  Pill.destroy(params[:id])
+  redirect_to "/pills"
+end
+
   private
 
-  def pills_params
+  def pill_params
     params.require(:pill).permit(:name :message)
   end
 

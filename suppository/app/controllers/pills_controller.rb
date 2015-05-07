d916d886acc256d7990702d5022d0179e5298a40
Tom Taylor <tom.taylor@outlook.com>
2015-05-07 14:31:53 -0500

    rails started with some stuff

---
+++ suppository/app/controllers/pills_controller.rb
@@ -0,0 +1,17 @@
+class PillsController < ApplicationController
+  def index
+    #Anna do
+  end
+
+  def show
+    #Tom do
+  end
+
+  def new
+    #Tom do
+  end
+
+  def edit
+    #Matt do
+  end
+end

16f57ff36196260b57ae74c15a59752ed4f40de1
Tom Taylor <tom.taylor@outlook.com>
2015-05-07 15:23:27 -0500

    Fixing

--- suppository/app/controllers/pills_controller.rb
+++ suppository/app/controllers/pills_controller.rb
@@ -7,14 +7,9 @@ class PillsController < ApplicationController
 
   def show
     #Tom do
-    @pill = Pill.find(params[:id])
   end
 
   def new
-<<<<<<< HEAD
-    #Tom
-=======
->>>>>>> 8e4087144d8857d752d69abc8364ca98099cf9dd
     @pill = Pill.new
   end
 
@@ -23,13 +18,10 @@ class PillsController < ApplicationController
     #Matt do
   end
 
-<<<<<<< HEAD
-=======
   private
 
   def pills_params
     params.require(:pill).permit(:name :message)
   end
 
->>>>>>> 8e4087144d8857d752d69abc8364ca98099cf9dd
 end
